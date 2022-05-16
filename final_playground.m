8e273e8c3b59df381b972d85d60da8a418e8b2b5
Ty Heim <96088336+TysonHeim@users.noreply.github.com>
2022-05-16 13:37:49 -0700

    Update final_playground.m

--- final_playground.m
+++ final_playground.m
@@ -1,5 +1,7 @@
+#Clean up mess
 clear all
 
+#setting range of both real and imaginary numbers as well the the tolerance for newtons method and the resolution of the final image.
 settings.realmin = -1.5;
 settings.realmax = 1.5;
 settings.imagmin = -1.5;
@@ -12,9 +14,10 @@ settings.tol = 1e-8;
 #          5.*x.^4 + 2.*x - 1];
 #f = @(x) [x.^3 - 1;
 #          3.*x.^2];
-
+#Function that will be ran through Newtons method
 f = @(x) x.^5 + x.^2 - x + 1;
 #f = @(x) x.^3 - 1;
+#create pairs of points in f to their derivatives using secant method
 f = createf(f);
 
 
@@ -26,16 +29,16 @@ f = createf(f);
 #f = @(x) [x.^2 - 1;
 #          2.*x];
 
-          
+#timer for sake of comparing our efficency 
 start = time();
 printf("Iterating...\n");
-[out, roots] = iterate(f, settings);
+[out, roots] = iterate(f, settings); #iterate through newtons method for each point 
 printf("comp -> hsl...\n");
-hsl = comp2hsl(out, roots, settings);
+hsl = comp2hsl(out, roots, settings); #convert roots into HSL
 printf("hsl -> rgb...\n");
-rgb = hsl2rgb(hsl);
+rgb = hsl2rgb(hsl); #concert HSL into RGB
 printf("exporting...\n");
-imwrite(rgb, "test.png");
+imwrite(rgb, "test.png"); #turn RGB of pixels into image
 printf("box dims...\n");
-boxDimensions(out)
-printf("Completed in %d seconds\n", time() - start);
+boxDimensions(out) #Calculate box dimension
+printf("Completed in %d seconds\n", time() - start); #print how long it took
