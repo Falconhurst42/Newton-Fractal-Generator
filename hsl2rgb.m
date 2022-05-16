be7773d58f6f4ad3f18bcade3977ae35b060cfaa
Ty Heim <96088336+TysonHeim@users.noreply.github.com>
2022-05-16 13:30:21 -0700

    Update hsl2rgb.m

--- hsl2rgb.m
+++ hsl2rgb.m
@@ -1,11 +1,12 @@
-
+#Converts a matrix of hsl values into rgb so mat;ab can create an image
 #take [hvec; svec; lvec]
 # ret [rvec; gvec; bvec]
 function rgb = hsl2rgb(hsl)
-  [n,m,l] = size(hsl);
-  hvec = hsl(:,:,1);
-  svec = hsl(:,:,2);
-  lvec = hsl(:,:,3);
+  [n,m,l] = size(hsl); 
+  hvec = hsl(:,:,1); #pull out hues
+  svec = hsl(:,:,2); #pull out saturations
+  lvec = hsl(:,:,3); #pull out brightnesses
+  #C X and M created via these definitions https://www.rapidtables.com/convert/color/hsl-to-rgb.html
   cvec = (1 - abs(2.*lvec - 1)) .* svec;
   xvec = cvec .* (1 - abs(mod(hvec./60, 2) - 1));
   mvec = lvec - cvec./2;
@@ -20,6 +21,7 @@ function rgb = hsl2rgb(hsl)
   # reduce hvec to categories
   hvec = floor(hvec./60)+1;
   
+  #bool masks 
   RX = MR(hvec) == 2;
   RC = MR(hvec) == 1;
   GX = MG(hvec) == 2;
@@ -27,9 +29,11 @@ function rgb = hsl2rgb(hsl)
   BX = MB(hvec) == 2;
   BC = MB(hvec) == 1;
   
+  #piecewise function from https://www.rapidtables.com/convert/color/hsl-to-rgb.html but in matrix algebra using boolean masks
   rvec = xvec.*RX + cvec.*RC;
   gvec = xvec.*GX + cvec.*GC;
   bvec = xvec.*BX + cvec.*BC;
   
+  #return the concatinated RGB values for each pixel
   rgb = cat(3, rvec + mvec, gvec + mvec, bvec + mvec);
 end
