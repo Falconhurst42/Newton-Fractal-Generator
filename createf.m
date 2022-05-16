f32969190d472cf78bb8c640f437637ac620d30f
Ty Heim <96088336+TysonHeim@users.noreply.github.com>
2022-05-16 13:32:02 -0700

    Update createf.m

--- createf.m
+++ createf.m
@@ -1,4 +1,5 @@
+#Creates f(x}, f'(x) pairs using secant method using matrix algebra since it is faster than for loops
 function pair_f = createf(f)
-  h = 1e-10;
+  h = 1e-10; #tolerance for secant method
   pair_f = @(x) [f(x); secant(f, x, h)];
 end
