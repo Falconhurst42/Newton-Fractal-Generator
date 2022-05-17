07d2ed390d4198fbec0f325484b6bea282c148a6
Ethan Worth <66226015+Falconhurst42@users.noreply.github.com>
2022-05-16 17:14:41 -0700

    fixed efficiency bottleneck

--- make_v.m
+++ make_v.m
@@ -3,17 +3,13 @@
 # Input:   settings - image size, min and max for both real and imaginary numbers -> complex plain numbers
 # Output:  v - a vector of n^2 complex numbers
 function v = make_v(settings)
-  # creating initial vectors for real and imaginary numbers
-  real = [];
-  imag = [];
-  # iterate until reaching resolution (from settings)
-  for r = 1:settings.resolution
-    # creating a row for every row in resolution
-    real = [real; linspace(settings.realmin, settings.realmax, settings.resolution)];
-    imag = [imag; linspace(settings.imagmin, settings.imagmax, settings.resolution)];
-  end
+  # creating initial matrices for real and imaginary numbers
+  real = linspace(range.realmin, range.realmax, range.resolution) .* ones(range.resolution);
+  imag = linspace(range.imagmin, range.imagmax, range.resolution) .* ones(range.resolution);
+  
   # adding together to create a matrix of complex numbers
   mat = real + i*imag';
+  
   # reshaping this matrix into vector with n x n values
   v = reshape(mat,1,[]);
 end
