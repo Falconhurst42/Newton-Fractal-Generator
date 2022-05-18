0862f8666a4aaeae0e39b6c53cd745813370bf1d
Ethan Worth <66226015+Falconhurst42@users.noreply.github.com>
2022-05-18 16:23:42 -0700

    Update createf.m

--- createf.m
+++ createf.m
@@ -1,5 +1,6 @@
-#Creates f(x}, f'(x) pairs using secant method using matrix algebra since it is faster than for loops
+#Creates redefines f(x) to produce [f(x), f'(x)] pairs where f'(X) is calculated using threePtCenterDiff method
+# this is fsater for cacluation, and allows the iterate method to take explicit or estimated derivatives
 function pair_f = createf(f)
-  h = 1e-10; #tolerance for secant method
+  h = 1e-10; #tolerance for threePtCenterDiff method
   pair_f = @(x) [f(x); threePtCenterDiff(f, x, h)];
 end
